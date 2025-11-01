f251842959587e3799ce6fc09d592c4886dfdf03
Engy Mohamed <Engybentsallam246200@gmail.com>
2025-11-02 00:34:22 +0200

    Refactor Assgiment01DStruc for clarity and efficiency

--- Assgiment01DStruc/src/assgiment01dstruc/Assgiment01DStruc.java
+++ Assgiment01DStruc/src/assgiment01dstruc/Assgiment01DStruc.java
@@ -1,105 +1,106 @@
 package assgiment01dstruc;
-
 import java.util.ArrayList;
 
-public class Assgiment01DStruc extends ExerciseSix {
+public class Assgiment01DStruc extends ExerciseSix{
+
     public static void main(String[] args) {
-        System.out.println("Question One:");
-        int[] numbers = {1, 3, 5, 7, 9};
-        int target = 5;
-        int index = -1;
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == target) {
-                index = i;
-                break;
-            }
+        // Question One
+        System.out.println("Qustion One:");
+        int[] arr1 = {2, 4, 6, 7, 9};
+        int sum = 0;
+        for (int num : arr1) {
+            sum += num;
         }
+        System.out.println("Sum of elements = " + sum);
+        System.out.println("******************************");
 
-        if (index != -1) {
-            System.out.println("Found at index: " + index);
-        } else {
-            System.out.println("Not found");
-        }
-
-        ///////////////////////////////////////////////////////////////////
-        System.out.println("\nQuestion Two:");
-        int[] numberss = {2, 4, 6, 7, 9};
-        int Sum1 = 0;
-        for (int num : numberss) {
-            Sum1 += num;
-        }
-        System.out.println("Sum of array elements: " + Sum1);
-
-        ///////////////////////////////////////////////////////////////////
-        System.out.println("\nQuestion Three:");
-        int[] arr = {1, 2, 1, 3, 5, 1};
-        int key = 1;
-        int[] result = new int[arr.length];
+        // Question Two
+        System.out.println("Qustion Two:");
+        int[] arr2 = {1, 2, 2, 3, 3, 3, 4, 5};
+        int[] temp = new int[arr2.length];
         int j = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != key) {
-                result[j++] = arr[i];
+        for (int i = 0; i < arr2.length - 1; i++) {
+            if (arr2[i] != arr2[i + 1]) {
+                temp[j++] = arr2[i];
             }
         }
-
-        System.out.println("*Input: \nArray = [1, 2, 1, 3, 5, 1]\nKey = " + key);
-        System.out.print("*Output: \nArray after removing Key: [");
+        temp[j++] = arr2[arr2.length - 1];
+        System.out.print("Array after removing duplicates: [");
         for (int i = 0; i < j; i++) {
-            System.out.print(result[i]);
-            if (i < j - 1) {
-                System.out.print(", ");
-            }
+            System.out.print(temp[i]);
+            if (i < j - 1) System.out.print(", ");
         }
         System.out.println("]");
+        System.out.println("******************************");
 
-        ///////////////////////////////////////////////////////////////////
-        System.out.println("\nQuestion Four:");
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(2);
-        list.add(4);
-        list.add(6);
-        list.add(7);
-        list.add(9);
-
-        int sum2 = 0;
-        for (int num : list) {
-            sum2 += num;
+        // Question Three
+        System.out.println("Qustion Three:");
+        int[] arr3 = {1, 2, 1, 3, 5, 1};
+        int key = 1;
+        int[] result = new int[arr3.length];
+        int k = 0;
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] != key) {
+                result[k++] = arr3[i];
+            }
         }
-        System.out.println("Sum of ArrayList elements: " + sum2);
-
-        ///////////////////////////////////////////////////////////////////
-        System.out.println("\nQuestion Five:");
-        ArrayList<Integer> numbersList = new ArrayList<>();
-        numbersList.add(2);
-        numbersList.add(4);
-        numbersList.add(6);
-        numbersList.add(7);
-        numbersList.add(9);
-
-        int sum3 = 0;
-        for (int num : numbersList) {
-            sum3 += num;
+        System.out.print("Array after removing " + key + ": [");
+        for (int i = 0; i < k; i++) {
+            System.out.print(result[i]);
+            if (i < k - 1) System.out.print(", ");
         }
-        System.out.println("Sum of ArrayList elements: " + sum3);
+        System.out.println("]");
+        System.out.println("******************************");
 
-        ///////////////////////////////////////////////////////////////////
-        System.out.println("\nQuestion Six:");
-        ArrayList<Integer> array = new ArrayList<>();
-        array.add(1);
-        array.add(2);
-        array.add(1);
-        array.add(3);
-        array.add(5);
-        array.add(1);
+        // Question Four
+        System.out.println("Qustion Four:");
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(2);
+        list1.add(4);
+        list1.add(6);
+        list1.add(7);
+        list1.add(9);
+        int sumList = 0;
+        for (int num : list1) {
+            sumList += num;
+        }
+        System.out.println("Sum of elements (ArrayList) = " + sumList);
+        System.out.println("******************************");
 
-        int keyy = 1;
-        // استدعاء الدالة من ExerciseSix
-        ArrayList<Integer> output = getElementsAfterKey(array, keyy);
+        // Question Five
+        System.out.println("Qustion Five:");
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(2);
+        list2.add(2);
+        list2.add(3);
+        list2.add(3);
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
 
-        System.out.println("Input array: " + array);
-        System.out.println("Key: " + keyy);
-        System.out.println("Output: " + output);
+        ArrayList<Integer> noDup = new ArrayList<>();
+        for (int i = 0; i < list2.size(); i++) {
+            if (i == 0 || !list2.get(i).equals(list2.get(i - 1))) {
+                noDup.add(list2.get(i));
+            }
+        }
+        System.out.println("List after removing duplicates: " + noDup);
+        System.out.println("******************************");
+
+        // Question Six
+        System.out.println("Qustion Six:");
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.add(1);
+        list3.add(2);
+        list3.add(1);
+        list3.add(3);
+        list3.add(5);
+        list3.add(1);
+
+        int key2 = 1;
+        ArrayList<Integer> filtered = ExerciseSix.removeAllOccurrencesList(list3, key2);
+        System.out.println("List after removing " + key2 + ": " + filtered);
     }
 }
